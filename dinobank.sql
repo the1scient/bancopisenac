65d3715ccd2925a4b8be74449d3364658a83dd13
Guilherme Fabbri <49078426+the1scient@users.noreply.github.com>
2022-11-03 22:19:52 -0300

    Add files via upload

--- dinobank.sql
+++ dinobank.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 02-Set-2022 às 03:30
+-- Tempo de geração: 04-Nov-2022 às 02:17
 -- Versão do servidor: 10.4.24-MariaDB
 -- versão do PHP: 8.1.6
 
@@ -32,19 +32,44 @@ CREATE TABLE `usuarios` (
   `email` varchar(100) CHARACTER SET utf8 NOT NULL,
   `nome` varchar(100) CHARACTER SET utf8 NOT NULL,
   `conta` varchar(50) CHARACTER SET utf8 NOT NULL,
-  `telefone` varchar(100) CHARACTER SET utf8 NOT NULL,
   `cpf` varchar(50) CHARACTER SET utf8 NOT NULL,
   `endereco` varchar(500) CHARACTER SET utf8 NOT NULL,
-  `senha` varchar(100) CHARACTER SET utf8 NOT NULL
+  `senha` varchar(100) CHARACTER SET utf8 NOT NULL,
+  `conta_tipo` enum('poupanca','corrente') NOT NULL DEFAULT 'poupanca',
+  `saldo` double(12,2) NOT NULL DEFAULT 0.00
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Extraindo dados da tabela `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `email`, `nome`, `conta`, `telefone`, `cpf`, `endereco`, `senha`) VALUES
-(1, 'mateus@gmail.com', 'Mateus Rodrigues de Oliveira', '00001-1', '40028922', '12345678900', 'Rua das Bananas, 123', '123'),
-(2, 'fernando@gmail.com', 'Fernando Moraes', '00002-1', '23498273482', '812618271281', 'Rua Itu, 123', '190');
+INSERT INTO `usuarios` (`id`, `email`, `nome`, `conta`, `cpf`, `endereco`, `senha`, `conta_tipo`, `saldo`) VALUES
+(1, 'guilherme@gmail.com', 'Guilherme Fabbri', '00001-1', '12345678900', 'Rua das Bananas, 123', '202cb962ac59075b964b07152d234b70', 'poupanca', 999.00),
+(2, 'pedro@gmail.com', 'Pedro Regado', '00002-1', '81261827128', 'Rua Itu, 123', 'cfecdb276f634854f3ef915e2e980c31', 'poupanca', 10.00),
+(3, 'mateus.mrdo24@gmail.com', 'Mateus Rodrigues de Oliveira', '', '24696924601', 'Avenida dos Travecos, 157', 'f9abb23c8c32ee1e7e6d62c185ffedfa', 'poupanca', 0.00),
+(4, 'anamacedo@gmail.com', 'Ana Beatriz', '', '50250075877', 'Avenida dos Travecos 12', 'd34bf12d54dc4c2a57bc34384bfd976c', 'poupanca', 234567890.00),
+(5, 'marina2020@gmail.com', 'marina da silva neto', '', '25843942813', 'rua da delicias', 'd34bf12d54dc4c2a57bc34384bfd976c', 'poupanca', 678967890.00),
+(6, 'fernando@gmail.com', 'Fernando Moraes', '', '11111111111', 'Avenida asdsadsa', '202cb962ac59075b964b07152d234b70', 'poupanca', 0.00),
+(7, 'rafaelronaldo@gmail.com', 'Rafael dos Santos', '', '48397729836', 'Avenida dos Travecos 123', 'd34bf12d54dc4c2a57bc34384bfd976c', 'poupanca', 11111110.00);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuarios_telefones`
+--
+
+CREATE TABLE `usuarios_telefones` (
+  `id_usuario` int(255) NOT NULL,
+  `telefone` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `usuarios_telefones`
+--
+
+INSERT INTO `usuarios_telefones` (`id_usuario`, `telefone`) VALUES
+(1, '11960337050'),
+(2, '11967246656');
 
 --
 -- Índices para tabelas despejadas
@@ -57,6 +82,12 @@ ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Índices para tabela `usuarios_telefones`
+--
+ALTER TABLE `usuarios_telefones`
+  ADD UNIQUE KEY `id_usuario` (`id_usuario`);
+
+--
 -- AUTO_INCREMENT de tabelas despejadas
 --
 
@@ -64,7 +95,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
